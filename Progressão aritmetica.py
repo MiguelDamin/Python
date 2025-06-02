47dede1d44f1fb313c2bd1b169604d15fdee5e11
Miguel Soares Damin <miguelmiguelitoda@gmail.com>
2025-06-02 20:01:16 -0300

    coi

---
+++ Progressão aritmetica.py
@@ -0,0 +1,9 @@
+"""A fórmula de progressão aritmética é: Termo1 + 9 * Razao, onde Termo1 é o primeiro valor da progressão e
+Razao é a diferença entre os termos.
+Faça um algoritmo que resolva a fórmula acima."""
+
+termo1 = float(input("Selecione o termo1: "))
+razao = float(input("Digite a razão: "))
+solucao = float
+solucao = termo1 + 9 * razao
+print("A solução da progressão aritimetica é: ", solucao)

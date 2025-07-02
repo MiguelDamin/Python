61f3fbbfc13eac216be5f3424f208107ad7e2782
Miguel Soares Damin <miguelmiguelitoda@gmail.com>
2025-07-01 21:53:15 -0300

    Update main.py

--- Projeto - Cadastro De Clientes/main.py
+++ Projeto - Cadastro De Clientes/main.py
@@ -8,18 +8,17 @@ conexao = mysql.connector.connect(
   )
 
 cursor = conexao.cursor()
-print("........................................................................................................................................................................................................................................................")
 escolha = int(input("____________________________________1-CADASTRAR NOVOS CLIENTES, 2-LISTAR, 3-REMOVER________________________________________ \n Selecione a Opção: "))
 
 if escolha == 1:
     nomePesquisado = input("NOME DO COMPLETO DO CLIENTE: ")
-    cursor.execute(f"INSERT INTO clientes(nome) VALUES('{nomePesquisado}')") #Insere um novo Usuário ao Banco
+    cursor.execute("INSERT INTO clientes(nome) VALUES(%s)", (nomePesquisado,)) #Insere um novo Usuário ao Banco
     conexao.commit() #Manda de Fato a Informação pro Banco
 elif escolha == 2:
     tipoDeSelect = int(input("____________________________________1-LISTAR CLIENTE ESPECÍFICO, 2-LISTAR TODOS____________________________________________ \n Selecione a Opção: "))
     if tipoDeSelect == 1:
         idPesquisa = int(input("DIGITE O ID DO CLIENTE: "))
-        cursor.execute(f"SELECT nome FROM clientes WHERE id_cliente = {idPesquisa}")
+        cursor.execute("SELECT nome FROM clientes WHERE id_cliente = %s", (idPesquisa,)) #Usei o %s para evitar SQL Injection
         resultados = cursor.fetchall()
         for linha in resultados:
             print(linha)
@@ -30,7 +29,7 @@ elif escolha == 2:
             print(linha)
 elif escolha == 3:
     idPesquisa = int(input("DIGITE O ID DO CLIENTE NO QUAL VOCÊ DESEJA REMOVER: "))
-    cursor.execute(f"DELETE FROM clientes WHERE id_cliente = {idPesquisa}")
+    cursor.execute("DELETE FROM clientes WHERE id_cliente = %s", (idPesquisa,))
     conexao.commit()
 
 

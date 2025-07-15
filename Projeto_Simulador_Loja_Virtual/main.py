e41d1c45f9e6388e3522f27904f0a146ce3839c0
Miguel Soares Damin <miguelmiguelitoda@gmail.com>
2025-07-14 21:09:39 -0300

    COMITAO 3.0

--- Projeto_Simulador_Loja_Virtual/main.py
+++ Projeto_Simulador_Loja_Virtual/main.py
@@ -50,24 +50,58 @@ if produto in idsDisponiveis:
    #E no códgio o usuario verifica o nome dele, se o nome dele não estiver cadastrado, o cod pula para a parte do cadastro e manda pro banco os dados
    cursor.execute("SELECT nome from clientes")
    nomesCadastrados = [linha[0].strip().lower() for linha in cursor.fetchall()]
-   confirmacaoCadastro = input(Fore.CYAN + "VOCÊ TEM CADASTRO NA LOJA DO ZE?(s/n): ").strip().lower()
+   while True:
+       confirmacaoCadastro = input(Fore.CYAN + "VOCÊ TEM CADASTRO NA LOJA DO ZE?(s/n): ").strip().lower()
+       if all(parte.isalpha() for parte in confirmacaoCadastro.split()):
+           break
+       print(Fore.YELLOW + "Erro: Confirme com (s) para SIM e (n) para não, não use números")
    if confirmacaoCadastro == "s" or confirmacaoCadastro == "sim":
-       confirmacaoNome = input("DIGITE SEU NOME CADASTRADO: ").strip().lower()
+       while True:
+           confirmacaoNome = input("DIGITE SEU NOME CADASTRADO: ").strip().lower()
+           if all(parte.isalpha() for parte in confirmacaoNome.split()):
+               break
+           else:
+              print("Seu nome deve ser escrito por letras, não números. Seu animal")
        if confirmacaoNome in nomesCadastrados:
            print(emoji.emojize(Fore.GREEN + "VOCÊ ESTÁ CADASTRADO :smiley:, BOA COMPRA", language='alias')) #Com Adição de Emoji
+           #Parte do código que simula a compra, não tem banco para isso, pois esses dados não podem ser registrados
+           print(Fore.YELLOW + "INICIANDO PAGAMENTO...")
+           numerodocartao = int(input(Fore.LIGHTBLUE_EX + "DIGITE O NÚMERO DO SEU CARTÃO: "))
+           dataExpiracao = int(input(Fore.LIGHTBLUE_EX + "DIGITE A DATA DE EXPIRAÇÃO DO CARTÃO: "))
+           codSeguranca = int(input(Fore.LIGHTBLUE_EX + "DIGITE O CÓDIGO DE SEGURANÇA DO CARTÃO: "))
    else:
         print(emoji.emojize(Fore.YELLOW + "VOCÊ AINDA NÃO TEM CADASTRO :cry:, CADASTRE-SE ABAIXO :smiley: ", language='alias'))
-        nomeDoCliente = input("DIGITE SEU NOME: ")
-        rua = input("DIGITE SUA RUA: ")
-        numeroCasa = input("DIGITE O NÚMERO DA CASA: ")
-        telefone = input("DIGITE SEU TELEFONE: ")
+        while True:
+           nomeDoCliente = input(Fore.LIGHTGREEN_EX + "DIGITE SEU NOME: ")
+           if all(parte.isalpha() for parte in nomeDoCliente.split()):
+               break
+           print(Fore.RED + "Erro: Digite letras, sem números ou símbolos")
+        while True:
+          rua = input(Fore.LIGHTGREEN_EX + "DIGITE SUA RUA: ")
+          if all(parte.isalpha() for parte in rua.split()):
+              break
+          print(Fore.RED + "Erro: Digite letras, sem números ou símbolos") 
+        while True:
+           numeroCasa = input(Fore.LIGHTGREEN_EX + "DIGITE O NÚMERO DA CASA: ")
+           if numeroCasa.isdigit():
+               break
+           print(Fore.RED + "Erro: Digite números, sem letras ou símbolos") 
+           
+        while True:
+          telefone = input(Fore.LIGHTGREEN_EX + "DIGITE SEU TELEFONE: ")
+          if telefone.isdigit():
+              break
+          print(Fore.RED + "Erro: Digite números, sem letras ou símbolos") 
         cursor.execute("INSERT INTO clientes(nome, rua, numerodacasa, telefone) VALUES(%s, %s, %s, %s)", (nomeDoCliente, rua, numeroCasa, telefone,))
         conexao.commit()
+        print(Fore.GREEN + "------> CADASTRO FEITO <------")
+        #Parte do código que simula a compra, não tem banco para isso, pois esses dados não podem ser registrados
+        print(Fore.YELLOW + "INICIANDO PAGAMENTO...")
+        numerodocartao = int(input(Fore.LIGHTBLUE_EX + "DIGITE O NÚMERO DO SEU CARTÃO: "))
+        dataExpiracao = int(input(Fore.LIGHTBLUE_EX + "DIGITE A DATA DE EXPIRAÇÃO DO CARTÃO: "))
+        codSeguranca = int(input(Fore.LIGHTBLUE_EX + "DIGITE O CÓDIGO DE SEGURANÇA DO CARTÃO: "))
 
-   #Parte do código que simula a compra, não tem banco para isso, pois esses dados não podem ser registrados
-   numerodocartao = int(input("DIGITE O NÚMERO DO SEU CARTÃO: "))
-   dataExpiracao = int(input("DIGITE A DATA DE EXPIRAÇÃO DO CARTÃO: "))
-   codSeguranca = int(input("DIGITE O CÓDIGO DE SEGURANÇA DO CARTÃO: "))
+   
 
   #Parte final do código, se o usuario realmente querer finaliar a compra, o registro de venda é mandado para
   #a tabela vendas, caso contrario cancela a compra e nada é de fato registrado no banco
